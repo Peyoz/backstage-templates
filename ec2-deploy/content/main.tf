795f5f8652da223561a067d8acf0a2859aca2907
PEYRACHIA Alessandro <alessandro.peyrachia@nposervices.com>
2024-12-16 14:42:30 +0100

    update

---
+++ ec2-deploy/content/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region     = var.awsRegion
+  #access_key = var.access_key
+  #secret_key = var.secret_key
+}
+
+resource "aws_instance" "example_server" {
+  ami           = "ami-059dd856d3b78700d"
+  instance_type = var.instanceType
+
+  tags = {
+    Name = var.instanceName
+  }
+}
